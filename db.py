cff41a23a16342f25254d8c4da082b55cd8ced3f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-11-19 20:16:03 +0530

    Update db.py

--- db.py
+++ db.py
@@ -11,8 +11,10 @@ cursor = mydb.cursor()
 
 
 def query_plate(plate_num):
-    pass
+    cursor.execute("SELECT * FROM vehicle WHERE plate_num = %s", (plate_num,))
+    return cursor.fetchall()
 
 
-def add_info():
-    pass
+def add_info(plate_num, owner_name, owner_phone, owner_address):
+    cursor.execute("INSERT INTO vehicle (plate_num, owner_name, owner_phone, owner_address) VALUES (%s, %s, %s, %s)",
+                   (plate_num, owner_name, owner_phone, owner_address))
